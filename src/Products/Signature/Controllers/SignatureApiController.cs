f984fb3ac8491d1e59ed87cc9062205a19b14826
Egor <egor.krushinov@aspose.com>
2020-04-09 10:33:28 +0300

    Changing the QR-code dimensions.

--- src/Products/Signature/Controllers/SignatureApiController.cs
+++ src/Products/Signature/Controllers/SignatureApiController.cs
@@ -641,7 +641,7 @@ namespace GroupDocs.Signature.MVC.Products.Signature.Controllers
                 SignatureDataEntity signaturesData = new SignatureDataEntity
                 {
                     ImageHeight = 200,
-                    ImageWidth = 270,
+                    ImageWidth = 200,
                     Left = 0,
                     Top = 0
                 };
